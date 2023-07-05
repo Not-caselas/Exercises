5abc3819d64c649173d99ef2ebf8b0aec4dd08df
Not-caselas <cosras@outlook.com>
2023-07-05 10:07:09 +0200

    Ejercicio 5

--- src/main/java/org/exercise/number_05/Main.java
+++ src/main/java/org/exercise/number_05/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
         String c = Input.string();
         System.out.println(c);
-        String[] name = c.split(",", 2);
+        String[] name = c.split(".", 2);
         for (String object: name){
             System.out.println(object);
         }
