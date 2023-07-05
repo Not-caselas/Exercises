2350a02f584c4424b67b37f8aebfa5e004ce6514
Not-caselas <cosras@outlook.com>
2023-07-05 09:24:16 +0200

    Ejercicio 7 (no acabado del todo)

--- src/main/java/org/exercise/number_07/Main.java
+++ src/main/java/org/exercise/number_07/Main.java
@@ -3,7 +3,7 @@ import org.exercise.Input.Input;
 
 import java.awt.*;
 import java.util.ArrayList;
-
+import java.util.List;
 public class Main {
     public static void main(String[] args){
             System.out.println("Choose an option:" +
@@ -11,17 +11,22 @@ public class Main {
             " s) sum the elements of the list" +
             " d) destroy the last element of the list");
 
-            ArrayList<String> array = new ArrayList<String>();
+            ArrayList<Double> array = new ArrayList<Double>();
             String input_1 = Input.string();
-            if (input_1.equals("a")){
+            if (input_1.equals("a")) {
                 String number = Input.string();
-                array.add(number);
-
-
+                array.add(Double.parseDouble(number));
             }
+            else if (input_1.equals("s")) {
+                Double sum = array.stream().mapToDouble(Double::doubleValue).sum();
+                System.out.println(sum);
+            }
+            else if (input_1.equals("d")) {
+                Integer i = ArrayList.size();
+                array.remove(i-1);
 
+           }
         }
-
 }
 
 
