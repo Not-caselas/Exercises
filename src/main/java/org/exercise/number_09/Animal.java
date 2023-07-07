1f0b63b0baef025a3cbf444981cec7e72fcbe9e8
Not-caselas <cosras@outlook.com>
2023-07-07 12:45:59 +0200

    Ejercicio 9

---
+++ src/main/java/org/exercise/number_09/Animal.java
@@ -0,0 +1,19 @@
+package org.exercise.number_09;
+
+public class Animal {
+    public String reino;
+    public String tipo;
+    public String raza;
+    public String medio;
+
+    public Animal(String reino,String tipo,String raza,String medio) {
+        this.reino = reino;
+        this.tipo = tipo;
+        this.raza = raza;
+        this.medio = medio;
+    }
+    @Override
+    public String toString() {
+        return "no sé que poner aquí";
+    }
+}
