904c886b1ab4816a2a5f6fc05ecc9f2d95a2b558
Not-caselas <cosras@outlook.com>
2023-07-07 08:45:48 +0200

    Ejercicio 5

--- src/main/java/org/exercise/number_04/Main.java
+++ src/main/java/org/exercise/number_04/Main.java
@@ -4,15 +4,20 @@ import org.exercise.Input.Input;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        ArrayList<Integer> numbers= new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         Integer c = Input.integer();
-        for (int i=c; i>=1; i--) {
+        for (int i = c+1; i >= 1; i--) {
             numbers.add(i);
-            }
-        for (int i=numbers.size();i>0;i--);
+        }
+        Integer j = 1;
+        for (int i = numbers.size() - 1; i > 0; i--) {
+            Integer result = j * numbers.get(i);
+            j=result;
+            System.out.println(result);
         }
 
+    }
 }
 
